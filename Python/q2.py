957419944ea0346438311e59340adc9330680587
Vincent Méthot <vincent.methot@gmail.com>
2014-04-03 14:44:31 -0400

    Retour aux espaces

--- Python/q2.py
+++ Python/q2.py
@@ -10,15 +10,15 @@ from dipy.core.ndindex import ndindex
 
 def tenseur(dmri, gtab):
 
-	dmri = nib.load(dmri)
-	data = dmri.get_data()
+    dmri = nib.load(dmri)
+    data = dmri.get_data()
 
-	print 'Forme de dmri:', data.shape
-	gtab = np.ndfromtxt(gtab)[1:]
-	S0 = data[..., 0]
-	S = data[..., 1:]
+    print 'Forme de dmri:', data.shape
+    gtab = np.ndfromtxt(gtab)[1:]
+    S0 = data[..., 0]
+    S = data[..., 1:]
 
-	B = gtab
+    B = np.array([ gtab[:,0]**2, gtab[:,0]*gtab[:,1], gtab[:,0]*gtab[:,2], gtab[:,1]**2, gtab[:,1]*gtab[:,2], gtab[:,2]**2 ])
 
-	for index in ndindex(data.shape[:3]):
-		X = -(1 / gtab[:, 3].astype(float)) * np.log( S[index].astype(float) / S0[index].astype(float) )
+    for index in ndindex(data.shape[:3]):
+        X = -(1 / gtab[:, 3].astype(float)) * np.log( S[index].astype(float) / S0[index].astype(float) )
