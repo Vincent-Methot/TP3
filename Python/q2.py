345a5571fc13a0dddd8effcd4a6997fbc1fc5b0b
Jeremie Fouquet <jeremie.fouquet2@gmail.com>
2014-04-03 15:37:05 -0400

    Fonctions pour carte de FA et ADC

--- Python/q2.py
+++ Python/q2.py
@@ -22,3 +22,33 @@ def tenseur(dmri, gtab):
 
     for index in ndindex(data.shape[:3]):
         X = -(1 / gtab[:, 3].astype(float)) * np.log( S[index].astype(float) / S0[index].astype(float) )
+
+
+
+def compAdcAndFa(tensMat):
+    """ Fonction qui calcule l'ADC et la fa d'une matrice 3D comprenant des
+    tenseurs de diffusion à chaque index."""
+
+    adcMap = np.zeros(tensMat.shape[:3])
+    faMap = np.zeros(tensMat.shape[:3])
+
+    for idx in ndindex(tensMat.shape[:3]):
+        dLin = tensMat[idx]
+        eigv = compLinDTensorEigval(dLin)
+        adcMap[idx] = eigv.sum() / 3
+        mEigv = eigv.mean()
+        faMap[idx] = np.sqrt(3/2*((eigv-eigv.mean())**2).sum()/(eigv**2).sum())
+
+    return adcMap, faMap
+
+
+
+def compDiffTenEigval(dLin):
+    dLin2MatIdx = np.array([[True, False, False],
+                            [True, True, False],
+                            [True, True, True]])
+    d = np.zeros(3, 3)
+    d[dLin2MatIdx] = dLin
+    eigv = np.linalg.eigvalsh(d)
+
+    return eigv
