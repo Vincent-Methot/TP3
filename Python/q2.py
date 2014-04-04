1385baad83859dedc87b9f1df8cffedc44211545
Vincent Méthot <vincent.methot@gmail.com>
2014-04-04 17:47:21 -0400

    Modifications mineures

--- Python/q2.py
+++ Python/q2.py
@@ -24,19 +24,21 @@ def tenseur(dmri, gtab):
     dmri = nib.load(dmri)
     data = dmri.get_data()
 
-    print 'Forme de dmri:', data.shape
     gtab = np.ndfromtxt(gtab)[1:]
     S0 = data[..., 0]
     S = data[..., 1:]
 
-    B = np.array([ gtab[:,0]**2, gtab[:,0]*gtab[:,1], gtab[:,0]*gtab[:,2], gtab[:,1]**2, gtab[:,1]*gtab[:,2], gtab[:,2]**2 ]).T
+    B = np.array([ gtab[:,0]**2, gtab[:,0]*gtab[:,1], gtab[:,0]*gtab[:,2],
+                   gtab[:,1]**2, gtab[:,1]*gtab[:,2], gtab[:,2]**2 ]).T
     tenseur = np.empty(data.shape[:3] + (6,))
 
     for index in ndindex(data.shape[:3]):
         if S0[index] == 0:
             tenseur[index] = np.zeros(6)
         else:
-            X = -(1 / gtab[:, 3].astype(float)) * ( np.log( S[index].astype(float) / S0[index].astype(float) ) )
+            X = -(1 / gtab[:, 3].astype(float)) * 
+                ( np.log( S[index].astype(float) / 
+                    S0[index].astype(float) ) )
             tenseur[index] = np.dot( np.linalg.pinv(B), X )
 
     tenseur[np.isinf(tenseur) | np.isnan(tenseur)] = 0
