516f89516c635d2b5510a67358b5c3cb6baf4a76
Vincent Méthot <vincent.methot@gmail.com>
2014-04-10 00:36:08 -0400

    Nettoyage de q2

--- Python/q2.py
+++ Python/q2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""TP3, question 2, IMN530"""
+"""TP3, question 2, IMN530. Par Jérémie Fouquet et Vincent Méthot"""
 
 import nibabel as nib
 import numpy as np
@@ -19,24 +19,29 @@ def tenseur(dmri, gtab):
 
     Retour
     ------
-    tenseur: nparray contenant les tenseurs à chaque voxel"""
+    tenseur: nparray contenant les tenseurs à chaque voxel
+
+    Example
+    -------
+    >> tenseur = q2.tenseur('Data/dmri.nii', 
+                 '../Data/gradient_directions_b-values.txt')"""
 
     dmri = nib.load(dmri)
     data = dmri.get_data()
 
-    print 'Forme de dmri:', data.shape
     gtab = np.ndfromtxt(gtab)[1:]
     S0 = data[..., 0]
     S = data[..., 1:]
 
-    B = np.array([ gtab[:,0]**2, gtab[:,0]*gtab[:,1], gtab[:,0]*gtab[:,2], gtab[:,1]**2, gtab[:,1]*gtab[:,2], gtab[:,2]**2 ]).T
+    B = np.array([ gtab[:,0]**2, gtab[:,0]*gtab[:,1], gtab[:,0]*gtab[:,2], 
+        gtab[:,1]**2, gtab[:,1]*gtab[:,2], gtab[:,2]**2 ]).T
     tenseur = np.empty(data.shape[:3] + (6,))
 
     for index in ndindex(data.shape[:3]):
         if S0[index] == 0:
             tenseur[index] = np.zeros(6)
         else:
-            X = -(1 / gtab[:, 3].astype(float)) * ( np.log( S[index].astype(float) / S0[index].astype(float) ) )
+            X = -(1 / gtab[:, 3].astype(float)) * ( np.log( S[index].astype(float) / S0[index].astype(float) ))
             tenseur[index] = np.dot( np.linalg.pinv(B), X )
 
     tenseur[np.isinf(tenseur) | np.isnan(tenseur)] = 0
@@ -64,7 +69,8 @@ def compAdcAndFa(tensMat):
         dLin = tensMat[idx]
         eigv = compLinDTensorEigval(dLin)
         adcMap[idx] = eigv.sum() / 3
-        faMap[idx] = np.sqrt(3/2*((eigv-eigv.mean())**2).sum()/(eigv**2).sum())
+        faMap[idx] = np.sqrt(3/2*((eigv-eigv.mean())**2).sum() / 
+                     (eigv**2).sum())
 
     return adcMap, faMap
 
