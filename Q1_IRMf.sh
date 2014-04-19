a0d3b8828a6e6ca49936960e7013f7df331595c1
Vincent Méthot <vincent.methot@gmail.com>
2014-04-19 12:03:45 -0400

    Maintenant, peut être vraiment lancé depuis la racine

--- Q1_IRMf.sh
+++ Q1_IRMf.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Script de reconstruction des zones d'activation les plus importantes
-# pour l'image Data/IRMf. Utilise les 'ideal time course' du fichier
-# Data/ideal.txt
+# pour l'image Data/IRMf.nii. Utilise la série temporelle idéalle du
+# fichier Data/ideal.txt
 
 # Nécessite les logiciels AFNI et FSL
 
 # Extraction du cerveau (fsl-bet)
-bet fmri.nii fmri_bet.nii.gz -F -f 0.6
+bet Data/fmri.nii Data/fmri_bet.nii.gz -F -f 0.6
 
 # Lissage spatial
-3dmerge -1blur_fwhm 6 -doall -prefix fmri_smooth.nii.gz fmri_bet.nii.gz
+3dmerge -1blur_fwhm 6 -doall -prefix Data/fmri_smooth.nii.gz Data/fmri_bet.nii.gz
 
 # Filtre passe-bas
-3dBandpass -prefix fmri_lowpass.nii.gz 0 .06 fmri_smooth.nii.gz
+3dBandpass -prefix Data/fmri_lowpass.nii.gz 0 .06 Data/fmri_smooth.nii.gz
 
 # Corrélation
-3dfim+ -input fmri_lowpass.nii.gz -ideal_file ideal.1D -out 'Correlation' -bucket fmri_corr.nii.gz
+3dfim+ -input Data/fmri_lowpass.nii.gz -ideal_file Data/ideal.txt -out 'Correlation' -bucket Data/fmri_corr.nii.gz
