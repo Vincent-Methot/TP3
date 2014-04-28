e97239023d947e9133a08f20d6b0dee77374a8be
Vincent Méthot <vincent.methot@gmail.com>
2014-04-28 08:33:59 -0400

    Trois format d'images sur lesquelles travailler

--- preprocess.py
+++ preprocess.py
@@ -1,21 +1,55 @@
 import nibabel as nib
 import numpy as np
+import Q2_IRMd
 
 dmri = nib.load('Data/dmri.nii')
 data = dmri.get_data()
 hdr = dmri.get_header()
 affine = dmri.get_affine()
 b0 = data[...,0]
-B0 = nib.Nifti1Image(b0, affine())
-nib.save(B0, 'b0.nii.gz')
+B0 = nib.Nifti1Image(b0, affine)
+nib.save(B0, 'Data/b0.nii.gz')
 
-# bet b0.nii.gz b0_bet.nii.gz -m
+# bet Data/b0.nii.gz Data/b0_bet.nii.gz -m
 
 mask = nib.load('Data/b0_bet_mask.nii.gz').get_data()
-dmri_brain = np.empty(data.shape)
+data_brain = np.empty(data.shape)
 
 # Pas la meilleure façon de faire, mais la plus simple...
 for i in range(data.shape[-1]):
-	dmri_brain[..., i] = data[..., i] * mask
+	data_brain[..., i] = data[..., i] * mask
 
+dmri_brain = nib.Nifti1Image(data_brain, affine, hdr)
+nib.save(dmri_brain, 'Data/dmri_brain.nii.gz')
 
+# Création de dmri_petit et dmri_mini
+
+data_petit = data[30:95, 20:100, 0:55, :]
+dmri_petit = nib.Nifti1Image(data_petit, affine)
+nib.save(dmri_petit, 'Data/dmri_petit.nii.gz')
+data_mini = data[50:70, 50:70, 20:40, :]
+dmri_mini = nib.Nifti1Image(data_mini, affine)
+nib.save(dmri_mini, 'Data/dmri_mini.nii.gz')
+
+# Calcul des tenseurs, FA et ADC
+
+tenseur_mini = Q2_IRMd.tenseur('Data/dmri_mini.nii.gz', 'Data/gradient_directions_b-values.txt')
+tenseur_petit = Q2_IRMd.tenseur('Data/dmri_petit.nii.gz', 'Data/gradient_directions_b-values.txt')
+tenseur = Q2_IRMd.tenseur('Data/dmri.nii', 'Data/gradient_directions_b-values.txt')
+
+nib.save(nib.Nifti1Image(tenseur_mini, affine), 'Data/tenseur_mini.nii.gz')
+nib.save(nib.Nifti1Image(tenseur_petit, affine), 'Data/tenseur_petit.nii.gz')
+nib.save(nib.Nifti1Image(tenseur, affine), 'Data/tenseur.nii.gz')
+
+FA_mini, ADC_mini = Q2_IRMd.compAdcAndFa(tenseur_mini)
+FA_petit, ADC_petit = Q2_IRMd.compAdcAndFa(tenseur_petit)
+FA, ADC = Q2_IRMd.compAdcAndFa(tenseur)
+
+nib.save(nib.Nifti1Image(FA_mini, affine), 'Data/FA_mini.nii.gz')
+nib.save(nib.Nifti1Image(FA_petit, affine), 'Data/FA_petit.nii.gz')
+nib.save(nib.Nifti1Image(FA, affine), 'Data/FA.nii.gz')
+nib.save(nib.Nifti1Image(ADC_mini, affine), 'Data/ADC_mini.nii.gz')
+nib.save(nib.Nifti1Image(ADC_petit, affine), 'Data/ADC_petit.nii.gz')
+nib.save(nib.Nifti1Image(ADC, affine), 'Data/ADC.nii.gz')
+
+# Tracking...
